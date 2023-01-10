f5d094f3dd30b79a63fce44fb7269242b1e7bef3
Atila jr <atilla.junior33@gmail.com>
2023-01-10 15:34:06 -0300

    Primeiro commit

---
+++ src/App.java
@@ -0,0 +1,27 @@
+import entities.Product;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import model.services.ProductService;
+
+public class App {
+
+  public static void main(String[] args) throws Exception {
+    System.out.println("===================================");
+    Locale.setDefault(Locale.US);
+    List<Product> list = new ArrayList<>();
+
+    list.add(new Product("Tv", 900.00));
+    list.add(new Product("Mouse", 50.00));
+    list.add(new Product("Tablet", 350.50));
+    list.add(new Product("HD Case", 80.90));
+
+    ProductService ps = new ProductService();
+
+    double sum = ps.filteredSum(list);
+
+    System.out.println("Sum = " + String.format("%.2f", sum));
+
+    System.out.println("===================================");
+  }
+}
